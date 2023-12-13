02f3e5dcc8a16b49ebd8f5c7c9b8aa65e5247af5
Alex Clay <aclay@mac.com>
2023-12-12 20:45:43 -0500

    Updating to Salt 3006.5

--- saltminion/tools/chocolateyInstall.ps1
+++ saltminion/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 # Python 3
-$url64_py3      = 'https://repo.saltproject.io/windows/Salt-Minion-3005.4-Py3-AMD64-Setup.exe'
-$checksum64_py3 = '4b4a08b59445199ebaa40a2a774e1df3'
-$url_py3        = 'https://repo.saltproject.io/windows/Salt-Minion-3005.4-Py3-x86-Setup.exe'
-$checksum_py3   = '54eecddc905e096212f717294b1d6071'
+$url64_py3      = 'https://repo.saltproject.io/salt/py3/windows/3006/Salt-Minion-3006.5-Py3-AMD64-Setup.exe'
+$checksum64_py3 = '7e0bea57616298b9edaf50ee1c7dd8f8c63291801a730d0aba0ea7f002ce6d3228c6772c2412f45235aedd8a02e2e47d256f1923ed6db948384244138ba6c7bd'
+$url_py3        = 'https://repo.saltproject.io/salt/py3/windows/3006/Salt-Minion-3006.5-Py3-x86-Setup.exe'
+$checksum_py3   = '72488721791ffc91b5cc095eb32b938ca1f77b357bf9c4c9703b5d44d09fa739ce4507584ca38b318059a202e6469ff5334628716d0cebae3f2e3dbfb51c9336'
 
 $packageArgs = @{
   packageName     = 'salt-minion'
@@ -15,11 +15,11 @@ $packageArgs = @{
 
   url             = $url_py3
   checksum        = $checksum_py3
-  checksumType    = 'md5'
+  checksumType    = 'sha512'
 
   url64bit        = $url64_py3
   checksum64      = $checksum64_py3
-  checksumType64  = 'md5'
+  checksumType64  = 'sha512'
 
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
