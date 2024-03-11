2f50996d1d6aedb51566805f5d766e40b534d9c1
Alex Clay <aclay@mac.com>
2024-03-11 07:43:09 -0400

    Bumping to 3007.0 and switching to stable download URLs

--- saltminion/tools/chocolateyInstall.ps1
+++ saltminion/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 # Python 3
-$url64_py3      = 'https://repo.saltproject.io/salt/py3/windows/3006/Salt-Minion-3006.7-Py3-AMD64-Setup.exe'
-$checksum64_py3 = '13ba9986657f063d4d521a4798a4fabf5594b2ffae3be57db4d7c025a4eca1a324e219068e33829181d734ba9957257d178a9488fbe93c8d48487966a0bab11c'
-$url_py3        = 'https://repo.saltproject.io/salt/py3/windows/3006/Salt-Minion-3006.7-Py3-x86-Setup.exe'
-$checksum_py3   = 'd6a0dd688cd90c010656cd94b2c091d79a6e72221f28ce52e5592a34248d256c331f04a181df6040e5ec074b2c6e9aeff28608e95f0f1ab6f1bea729c4bb3f10'
+$url64_py3      = 'https://repo.saltproject.io/salt/py3/windows/minor/3007.0/Salt-Minion-3007.0-Py3-AMD64-Setup.exe'
+$checksum64_py3 = '6a2af75807abaa0df0164e0edc11651153cf87ba201962141ef1c9a3c2e27750d4827f7b95e482afdb5f9cb02423d1b5d2c10e1bd1d90f24beae4b2544bb1016'
+$url_py3        = 'https://repo.saltproject.io/salt/py3/windows/minor/3007.0/Salt-Minion-3007.0-Py3-x86-Setup.exe'
+$checksum_py3   = 'a1cd10d9251fd6bc1fae50f3b1f050a02cb52b890b8c3560c328b2b7403642f5ab23f34e55216188ff3c38024d538cdc99bbf65cc2f6b3e889a668e58a321e49'
 
 $packageArgs = @{
   packageName     = 'salt-minion'
