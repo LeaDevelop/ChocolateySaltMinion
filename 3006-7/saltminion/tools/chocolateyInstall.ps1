2c72176cbc956d2d5cc3d20266b9a2b397b638cf
Lea N. <104503492+LeaDevelop@users.noreply.github.com>
2024-05-20 00:04:34 +0200

    - nuspec iconUrl, docsUrl, releaseNotes url updated - Corrected chocolateyInstall.ps1 url/64_py3 and checksum/64_py3 for saltminion 3006.7

---
+++ 3006-7/saltminion/tools/chocolateyInstall.ps1
@@ -0,0 +1,59 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Python 3
+$url64_py3      = 'https://repo.saltproject.io/salt/py3/windows/minor/3006.7/Salt-Minion-3006.7-Py3-AMD64-Setup.exe'
+$checksum64_py3 = '13BA9986657F063D4D521A4798A4FABF5594B2FFAE3BE57DB4D7C025A4ECA1A324E219068E33829181D734BA9957257D178A9488FBE93C8D48487966A0BAB11C'
+$url_py3        = 'https://repo.saltproject.io/salt/py3/windows/minor/3006.7/Salt-Minion-3006.7-Py3-x86-Setup.exe'
+$checksum_py3   = 'D6A0DD688CD90C010656CD94B2C091D79A6E72221F28CE52E5592A34248D256C331F04A181DF6040E5EC074B2C6E9AEFF28608E95F0F1AB6F1BEA729C4BB3F10'
+
+$packageArgs = @{
+  packageName     = 'salt-minion'
+  unzipLocation   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType        = 'EXE'
+
+  softwareName    = 'Salt*'
+
+  url             = $url_py3
+  checksum        = $checksum_py3
+  checksumType    = 'sha512'
+
+  url64bit        = $url64_py3
+  checksum64      = $checksum64_py3
+  checksumType64  = 'sha512'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters['Master']) {
+  $Master = $packageParameters['Master']
+  $packageArgs['silentArgs'] += " /master=$Master"
+}
+
+if ($packageParameters['MinionName']) {
+  $MinionName = $packageParameters['MinionName']
+  $packageArgs['silentArgs'] += " /minion-name=$MinionName"
+}
+
+if ($packageParameters['MinionStart']) {
+  $MinionStart = $packageParameters['MinionStart']
+  $packageArgs['silentArgs'] += " /start-minion=$MinionStart"
+}
+
+if ($packageParameters['MinionStartDelayed'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /start-minion-delayed"
+}
+
+if ($packageParameters['DefaultConfig'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /default-config"
+}
+
+if ($packageParameters['CustomConfig']) {
+  $CustomConfig = $packageParameters['CustomConfig']
+  $packageArgs['silentArgs'] += " /custom-config=$CustomConfig"
+}
+
+Install-ChocolateyPackage @packageArgs
