bea04b3ffdf43a9bca9fbfe941d5130ba09284ab
Alex Clay <alex@suran.com>
2025-05-15 07:52:57 -0400

    Add Salt 3007.2

---
+++ 3007-2/tools/chocolateyInstall.ps1
@@ -0,0 +1,59 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Python 3
+$url64_py3      = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3007.2/Salt-Minion-3007.2-Py3-AMD64-Setup.exe'
+$checksum64_py3 = '9d46f907cb744ec5c02bb0ee6ec79e9e7d8460b4d29cbe898c34610f747a3f43'
+$url_py3        = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3007.2/Salt-Minion-3007.2-Py3-x86-Setup.exe'
+$checksum_py3   = 'b4cc5ca01aa0c2251e97925b0dc707f2c3b58443cd7fa964c7c5fce0b1071a20'
+
+$packageArgs = @{
+  packageName     = 'salt-minion'
+  unzipLocation   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType        = 'EXE'
+
+  softwareName    = 'Salt*'
+
+  url             = $url_py3
+  checksum        = $checksum_py3
+  checksumType    = 'sha256'
+
+  url64bit        = $url64_py3
+  checksum64      = $checksum64_py3
+  checksumType64  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters['Master']) {
+  $Master = $packageParameters['Master']
+  $packageArgs['silentArgs'] += " /master=$Master"
+}
+
+if ($packageParameters['MinionName']) {
+  $MinionName = $packageParameters['MinionName']
+  $packageArgs['silentArgs'] += " /minion-name=$MinionName"
+}
+
+if ($packageParameters['MinionStart']) {
+  $MinionStart = $packageParameters['MinionStart']
+  $packageArgs['silentArgs'] += " /start-minion=$MinionStart"
+}
+
+if ($packageParameters['MinionStartDelayed'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /start-minion-delayed"
+}
+
+if ($packageParameters['DefaultConfig'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /default-config"
+}
+
+if ($packageParameters['CustomConfig']) {
+  $CustomConfig = $packageParameters['CustomConfig']
+  $packageArgs['silentArgs'] += " /custom-config=$CustomConfig"
+}
+
+Install-ChocolateyPackage @packageArgs
