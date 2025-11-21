b2152cfa531cf5f99cadb369242ec15144093970
Alex Clay <alex@suran.com>
2025-11-21 07:36:43 -0500

    Add 3006.17 and 3007.9 CVE versions of Salt

---
+++ 3006-17/tools/chocolateyInstall.ps1
@@ -0,0 +1,59 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Python 3
+$url64_py3      = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3006.17/Salt-Minion-3006.17-Py3-AMD64-Setup.exe'
+$checksum64_py3 = '33d0a847864c4596d111a7f6cb9025b245c73fd483d60f064be79f3e3b65823f'
+$url_py3        = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3006.17/Salt-Minion-3006.17-Py3-x86-Setup.exe'
+$checksum_py3   = 'f7fd0ba5f75332893a781027e2c42a0581ba2a9c11376e64c428909b4194a18c'
+
+$packageArgs = @{
+  packageName     = 'salt-minion'
+  unzipLocation   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType        = 'EXE'
+
+  softwareName    = 'Salt*'
+
+  url             = $url_py3
+  checksum        = $checksum_py3
+  checksumType    = 'sha256'
+
+  url64bit        = $url64_py3
+  checksum64      = $checksum64_py3
+  checksumType64  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters['Master']) {
+  $Master = $packageParameters['Master']
+  $packageArgs['silentArgs'] += " /master=$Master"
+}
+
+if ($packageParameters['MinionName']) {
+  $MinionName = $packageParameters['MinionName']
+  $packageArgs['silentArgs'] += " /minion-name=$MinionName"
+}
+
+if ($packageParameters['MinionStart']) {
+  $MinionStart = $packageParameters['MinionStart']
+  $packageArgs['silentArgs'] += " /start-minion=$MinionStart"
+}
+
+if ($packageParameters['MinionStartDelayed'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /start-minion-delayed"
+}
+
+if ($packageParameters['DefaultConfig'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /default-config"
+}
+
+if ($packageParameters['CustomConfig']) {
+  $CustomConfig = $packageParameters['CustomConfig']
+  $packageArgs['silentArgs'] += " /custom-config=$CustomConfig"
+}
+
+Install-ChocolateyPackage @packageArgs
